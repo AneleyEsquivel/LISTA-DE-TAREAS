f42e9b1c450357587ec3baa86ae918f0de69b4ed
AneleyEsquivel <aneleyesquivel@gmail.com>
2025-04-16 16:57:12 -0300

    botones

--- main.js
+++ main.js
@@ -2,14 +2,36 @@ const input = document.getElementById("agregar-tarea");
 console.log(input);
 const mas = document.getElementById("agregar");
 const lista = document.getElementById("lista1");
-const agregarTarea = (tarea) => {
+
+const hechos = 'fa-check-circle';
+const pendiente = 'fa-circle';
+const tachado = 'tachado';
+
+const agregarTarea = (tarea, check) => {
+    const estado = check ? hecho : pendiente;
+    const tachar = check ? tachado : "";
 const elemento = `<li>
-                    <i class="fas fa-circle co check" id="check" data-="check"></i>
-                    <p class="tareaok tachado" id="tareaok">${tarea}</p>
-                    <i class="fas fa-trash de borrar" id="borrar" data-="borrar"></i>
+                    <i class="far ${estado} check"  id="check" data="check"></i>
+                    <p class="tareaok ${tachar}" id="tareaok">${tarea}</p>
+                    <i class="fas fa-trash de borrar" id="borrar" data="borrar"></i>
                 
                 </li>`
                 lista.insertAdjacentHTML("beforeend", elemento);
+};
+
+const tareaRealizada = (element) => {
+    element.classList.toggle(hecho);
+    element.classList.toggle(pendiente);
+    element.parentNode.querySelector('.tarea').classList.taggle(tachado);
+};
+
+const tareaEliminada = (element) => {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+};
+
+const cambiarEtilos = () => {
+    const link = document.getElementById("estilo");
+    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
 };
 
 //LLAMO FUNCIÓN
@@ -34,9 +56,19 @@ document.addEventListener( "keyup", (e)=> {
     }
 })
 
-// LLAMAR A LA FUCIÓN CAMBIO DE ESTILO
+lista.addEventListener('click', function (event) {
+const element = event. target ;
+const elementData = element.atributes.data.value; 
+if (elementData == 'check') {
+    tareaRealizada (element)
 
-const cambiarEtilos = () => {
-    const link = document.getElementById("estilo");
-    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
-};
+}
+else if (elementData == 'borrar'){
+    tareaEliminada (element)
+}
+});
+
+
+
+
+
